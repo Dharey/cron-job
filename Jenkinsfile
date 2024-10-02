d2a5b5858094713d005140334fbb9c38ae6c6577
Oluwaseyi Awotedu <asa@grossp.onmicrosoft.com>
2024-10-02 17:18:30 +0100

    Updated files

---
+++ Jenkinsfile
@@ -0,0 +1,31 @@
+pipeline {
+  agent any
+	// trigger restart at 6:00 AM and 6:00 PM - Africa/Lagos Timezone
+	triggers {
+    cron('TZ=Africa/Lagos \n "* * * * *"') 
+	}
+  options {
+		skipStagesAfterUnstable()
+		disableConcurrentBuilds()
+		ansiColor('xterm')
+		parallelsAlwaysFailFast()
+  }
+  stages {
+		// stage('QA') {
+		// 	when {
+		// 		branch 'qa'
+		// 	}
+			stages {
+				stage('Execute restart pod script') {
+          agent {
+						any
+					}
+					steps {
+					  // sh 'sudo su -' // Switch to root user
+						sh 'pwd' // Print present working directory
+						sh 'timeout 5m sh $PWD/restart_rb.sh' // Execute restart_rb shell script
+					}
+				}
+ 			}
+        }
+  }
---
+++ restart_rb.sh
@@ -0,0 +1,18 @@
+# #!/bin/bash
+# Get all deployments in the acquiring namespace
+NAMESPACE="monitoring"
+ACQUIRING_JPTS_AND_NUS_PODS=$(kubectl get pods --namespace $NAMESPACE -o custom-columns=:metadata.name)
+# | grep -E 'jpos-acquiring-nus|jpos-acquiring-jpts'
+#)
+echo "The following pods in the acquiring namespace will be restarted:"
+for pod in $ACQUIRING_JPTS_AND_NUS_PODS; do
+  echo "Restarting pod: $ACQUIRING_JPTS_AND_NUS_PODS"
+  
+  # Delete the pod (which will cause it to be restarted)
+  kubectl delete pod $ACQUIRING_JPTS_AND_NUS_PODS -n $NAMESPACE
+  
+  # Wait for the pod to be recreated and in Ready state before moving to the next pod
+  kubectl wait --for=condition=Ready pod/$ACQUIRING_JPTS_AND_NUS_PODS -n $NAMESPACE --timeout=300s
+done
+
+echo "NUS and JPTS pods have been restarted sequentially."
